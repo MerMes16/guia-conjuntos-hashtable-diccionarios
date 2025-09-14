4d85ecf38cfb016ea13322208797729b7e44db10
MerMes16 <imerinomesa@gmail.com>
2025-09-14 13:56:10 -0300

    R ej Eliminar repetidos

--- ejercicios/conjuntos.go
+++ ejercicios/conjuntos.go
@@ -3,8 +3,11 @@ package ejercicios
 import "untref-ayp2/guia-conjuntos-hashes-diccionarios/set"
 
 func EliminarRepetidos[T comparable](array []T) []T {
-	// Implementar
-	return nil
+	set := set.NewListSet[T]()
+	for _, v := range array {
+		set.Add(v)
+	}
+	return set.Values()
 }
 
 func InterseccionMultiple[T comparable](sets ...*set.ListSet[T]) *set.ListSet[T] {
